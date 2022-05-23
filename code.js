23907c75d59af8a352ae810ba4e5acae4c438e97
Kighte <kighte04@gmail.com>
2022-05-23 15:52:14 +0200

    Language twist and rae/WR added

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
+
 function wordSearch() {
-	let word = document.querySelector(".text-input").value;
-    window.open(`https://www.google.com/search?q=¿+Qué+significa+${word}+?`);
+    let actualLang = document.querySelector(".form-parent").name;
+    if (actualLang == "english"){
+        let word = document.querySelector(".text-input").value;
+        window.open(`https://www.wordreference.com/es/translation.asp?tranword=${word}`);
+    }else if (actualLang == "español"){
+        let word = document.querySelector(".text-input").value;
+        window.open(`https://dle.rae.es/${word}?m=form`);
+    }
 }
-
-
 
