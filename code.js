798d6c9d6dd60bc319fc1d357c59bc77c0f89c80
Kighte <kighte04@gmail.com>
2022-05-23 16:40:23 +0200

    word

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ function wordSearch() {
     let actualLang = document.querySelector(".form-parent").name;
     if (actualLang == "english"){
         let word = document.querySelector(".text-input").value;
-        window.open(`https://www.wordreference.com/es/translation.asp?tranword=${word}`);
+        window.open(`https://www.merriam-webster.com/dictionary/${word}`);
     }else if (actualLang == "español"){
         let word = document.querySelector(".text-input").value;
         window.open(`https://dle.rae.es/${word}?m=form`);
